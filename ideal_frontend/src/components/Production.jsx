5f32b3b7c6473ebace55f7132596dd49bbca712a
domenicorizzi <rizzidomenico31@gmail.com>
2025-08-07 11:38:45 +0200

    Primo commit

---
+++ ideal_frontend/src/components/Production.jsx
@@ -0,0 +1,128 @@
+export default function Production() {
+    return (
+        <section className="text-gray-600 body-font">
+                <div className="text-center mt-20">
+                    <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">Il nostro processo produttivo</h1>
+                    <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Ogni progetto è seguito con attenzione, precisione e trasparenza in ogni fase.</p>
+                    <div className="flex mt-6 justify-center">
+                        <div className="w-16 h-1 rounded-full bg-indigo-500 inline-flex"></div>
+                    </div>
+                </div>
+                <div className="container px-5 py-24 mx-auto flex flex-wrap">
+                    <div className="flex flex-wrap w-full">
+                        <div className="lg:w-2/5 md:w-1/2 md:pr-10 md:py-6">
+                            <div className="flex relative pb-12">
+                                <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+                                    <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                                </div>
+                                <div
+                                    className="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                         className="size-6">
+                                        <path fillRule="evenodd"
+                                              d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                                              clipRule="evenodd"/>
+                                    </svg>
+
+                                </div>
+                                <div className="flex-grow pl-4">
+                                    <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">STEP
+                                        1 - RICHIESTA PREVENTIVO</h2>
+
+                                    <p className="leading-relaxed">Riceviamo la tua richiesta e valutiamo le specifiche
+                                        del progetto (quantità, formato, tipo di stampa).</p>
+                                </div>
+                            </div>
+                            <div className="flex relative pb-12">
+                                <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+                                    <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                                </div>
+                                <div
+                                    className="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                         className="size-6">
+                                        <path fillRule="evenodd"
+                                              d="M2.25 5.25a3 3 0 0 1 3-3h13.5a3 3 0 0 1 3 3V15a3 3 0 0 1-3 3h-3v.257c0 .597.237 1.17.659 1.591l.621.622a.75.75 0 0 1-.53 1.28h-9a.75.75 0 0 1-.53-1.28l.621-.622a2.25 2.25 0 0 0 .659-1.59V18h-3a3 3 0 0 1-3-3V5.25Zm1.5 0v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5Z"
+                                              clipRule="evenodd"/>
+                                    </svg>
+
+                                </div>
+                                <div className="flex-grow pl-4">
+                                    <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">STEP
+                                        2 - ANALISI TECNICA</h2>
+                                    <p className="leading-relaxed">Controlliamo i file, ti consigliamo i materiali e
+                                        ottimizziamo la stampa per qualità e costi.</p>
+                                </div>
+                            </div>
+                            <div className="flex relative pb-12">
+                                <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+                                    <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                                </div>
+                                <div
+                                    className="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                         className="size-6">
+                                        <path fillRule="evenodd"
+                                              d="M9 1.5H5.625c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0 0 16.5 9h-1.875a1.875 1.875 0 0 1-1.875-1.875V5.25A3.75 3.75 0 0 0 9 1.5Zm6.61 10.936a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 14.47a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+                                              clipRule="evenodd"/>
+                                        <path
+                                            d="M12.971 1.816A5.23 5.23 0 0 1 14.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 0 1 3.434 1.279 9.768 9.768 0 0 0-6.963-6.963Z"/>
+                                    </svg>
+
+                                </div>
+                                <div className="flex-grow pl-4">
+                                    <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">STEP
+                                        3 - PROVA E APPROVAZIONE</h2>
+                                    <p className="leading-relaxed">Ti forniamo una prova di stampa (digitale o fisica)
+                                        da approvare prima della produzione.</p>
+                                </div>
+                            </div>
+                            <div className="flex relative pb-12">
+                                <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+                                    <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                                </div>
+                                <div
+                                    className="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                         className="size-6">
+                                        <path fillRule="evenodd"
+                                              d="M7.875 1.5C6.839 1.5 6 2.34 6 3.375v2.99c-.426.053-.851.11-1.274.174-1.454.218-2.476 1.483-2.476 2.917v6.294a3 3 0 0 0 3 3h.27l-.155 1.705A1.875 1.875 0 0 0 7.232 22.5h9.536a1.875 1.875 0 0 0 1.867-2.045l-.155-1.705h.27a3 3 0 0 0 3-3V9.456c0-1.434-1.022-2.7-2.476-2.917A48.716 48.716 0 0 0 18 6.366V3.375c0-1.036-.84-1.875-1.875-1.875h-8.25ZM16.5 6.205v-2.83A.375.375 0 0 0 16.125 3h-8.25a.375.375 0 0 0-.375.375v2.83a49.353 49.353 0 0 1 9 0Zm-.217 8.265c.178.018.317.16.333.337l.526 5.784a.375.375 0 0 1-.374.409H7.232a.375.375 0 0 1-.374-.409l.526-5.784a.373.373 0 0 1 .333-.337 41.741 41.741 0 0 1 8.566 0Zm.967-3.97a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H18a.75.75 0 0 1-.75-.75V10.5ZM15 9.75a.75.75 0 0 0-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 0 0 .75-.75V10.5a.75.75 0 0 0-.75-.75H15Z"
+                                              clipRule="evenodd"/>
+                                    </svg>
+
+                                </div>
+                                <div className="flex-grow pl-4">
+                                    <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">STEP
+                                        4 - STAMPA E FINITURA</h2>
+                                    <p className="leading-relaxed">Procediamo con la stampa offset o digitale e le
+                                        eventuali finiture: taglio, plastificazione, piega, ecc.</p>
+                                </div>
+                            </div>
+                            <div className="flex relative">
+                                <div
+                                    className="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center text-white relative z-10">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                         className="size-6">
+                                        <path
+                                            d="M3.375 3C2.339 3 1.5 3.84 1.5 4.875v.75c0 1.036.84 1.875 1.875 1.875h17.25c1.035 0 1.875-.84 1.875-1.875v-.75C22.5 3.839 21.66 3 20.625 3H3.375Z"/>
+                                        <path fillRule="evenodd"
+                                              d="m3.087 9 .54 9.176A3 3 0 0 0 6.62 21h10.757a3 3 0 0 0 2.995-2.824L20.913 9H3.087Zm6.163 3.75A.75.75 0 0 1 10 12h4a.75.75 0 0 1 0 1.5h-4a.75.75 0 0 1-.75-.75Z"
+                                              clipRule="evenodd"/>
+                                    </svg>
+
+                                </div>
+                                <div className="flex-grow pl-4">
+                                    <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">STEP
+                                        5 - CONSEGNA</h2>
+                                    <p className="leading-relaxed">Prepariamo l’imballo e spediamo o consegniamo il
+                                        lavoro completo con puntualità e cura.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <img className="lg:w-3/5 md:w-1/2 object-cover object-center rounded-lg md:mt-0 mt-12"
+                             src="../public/offset.jpg" alt="step"/>
+                    </div>
+                </div>
+        </section>
+)
+}
