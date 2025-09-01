abb9ff088bd2eddcb3be95cf4411f9eb9ccbdada
domenicorizzi <rizzidomenico31@gmail.com>
2025-09-01 17:54:21 +0200

    404 p3

--- backend/server.js
+++ backend/server.js
@@ -94,9 +94,9 @@ app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // Configurazione Nodemailer
 const transporter = nodemailer.createTransport({
-    host: 'smtps.aruba.it',
-    port: 587,
-    secure: false,
+    host: 'smtp.aruba.it',
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS
